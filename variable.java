d44cc893ae9c183704c6e1a983ccdd62eec0647a
pownkumar-g <pownkumarofficial@gmail.com>
2024-12-27 21:52:06 +0530

    variables

---
+++ variable.java
@@ -0,0 +1,27 @@
+package java;
+
+public class variable {
+    public static void main (String[] args){
+        int a=10;
+        String name ="hellow";
+        double b= 10.0;
+        char words ='h';
+        float c= 19.99f;
+        int d=a+a;
+        System.out.println(a);
+        System.out.println(name);
+        System.out.println(b);
+        System.out.println(words);
+        System.out.println(c);
+        System.out.println(d);
+        int x,y,z;
+        x=y=z=50;
+        System.out.println(x+y+z);
+        int s=10,f=20,r=30;
+        System.out.println(s+f+r);
+
+    }
+
+
+    
+}
